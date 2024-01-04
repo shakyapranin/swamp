6ab4ef8ad7c305e29bdc9b842bcf929d44e05281
praneen <praneenshakya@lftechnology.com>
2024-01-04 14:53:42 +0545

    Dirty stripe implementation initialization
    - [x] Stripe initialization
    - [x] Jest setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-const express = require('express');
+import { StripeService } from "./services/StripeService";
+import express, { Request } from "express";
+
+const app = express();
+const stripeService = new StripeService('testStripeKey'); // HINT: Should the stripe key be static here? May be an environment variable should be used.
+
+app.post('/stripe/payment', (_req: Request) => {
+    const amount = _req.params.amount; // HINT: Should the parameters be validated first before access? May be use a request validator
+    const paymentMethod = _req.params.paymentMehod; // HINT: Should the parameters be validated first before access? May be use a request validator
+    return stripeService.processPayment(parseInt(amount), paymentMethod); // HINT: Should parsing of string to integer be done here? May be use a request validator as middleware to parse?
+});
---
+++ src/services/mocks/Stripe.ts
@@ -0,0 +1,27 @@
+import { STATUS_CODES } from "../../consts/STATUSCODES"; // HINT: Is it good to use absolute paths? May be use an alias here?
+
+export class Stripe {
+    stripeKey: string;
+    constructor(stripeKey: string) {
+        this.stripeKey = stripeKey;
+    }
+    processWalletPayment(amount: number): StripeResponse {
+        return {
+            statusCode: STATUS_CODES.OK,
+            message: `Wallet payment for ${amount} successfully processed.`
+        }
+    }
+
+    processCardPayment(amount: number): StripeResponse {
+        return {
+            statusCode: STATUS_CODES.OK,
+            message: `Card payment for ${amount} successfully processed.`
+        }
+    }
+
+}
+
+export interface StripeResponse {
+    statusCode: number;
+    message: string;
+}
